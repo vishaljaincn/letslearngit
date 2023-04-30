97c0cb2f93292816c1071ba0b624f7e8e7e40487
vishal jain <vishaljaincn@gmail.com>
2023-04-30 16:48:23 +0530

    modified sum.java file

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args) {
         int a=5;
         int b=6;
-        System.out.println(a+b);
+        System.out.println( "sum of 2 no"+ (a+b));
     }
 }
